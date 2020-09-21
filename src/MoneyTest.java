d80e1cb29acfaa308f84c2b38f872086f2e5f89e
alinebrito <nbrito.aline@gmail.com>
2020-09-21 17:24:12 -0300

    1) Add Dollar and Test classes

---
+++ src/MoneyTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void testMultiplication() {
+       Dollar five = new Dollar(5);
+       five.times(2);
+       assertEquals(10, five.amount);
+    }
+}
